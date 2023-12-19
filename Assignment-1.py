d4d7aa38e924e2152d296f348e2044d9a588613d
rose-257 <rosemariyajoy4@gmail.com>
2023-12-19 13:02:57 +0530

    message

---
+++ Assignment-1.py
@@ -0,0 +1,86 @@
+"""
+num=int(input("Enter the number:"))
+if(num%2==0):
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
+    
+
+
+a=int(input("Enter first Number:"))
+b=int(input("Enter second Number:"))
+c=int(input("Enter third Number:"))
+
+if(a>b and a>c):
+    print(a," is largest number")
+elif(b>a and b>c):
+     print(b," is largest number")
+else:
+     print(c," is largest number")
+
+
+
+
+year = int(input("Enter the year"))
+
+if(year%4==0 and year%100!=0):
+    print("Leap Year")
+elif(year%400==0 and year%100==0):
+    print("Leap Year")    
+else:
+    print("Non-Leap Year")
+
+
+
+num=int(input("Enter the number:"))
+sum=0
+while(num>0):
+    sum+=num
+    num-=1
+print("Sum is:" ,sum)
+
+
+
+
+
+
+num=int(input("Enter the number:"))
+while(num>0):
+    print(num);
+    num-=1;
+    
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
